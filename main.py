4e6dbd00b43ce537a2e466b2e484eab8b2e9dbfc
DDoS000 <Suphamongkhon.kh.61@ubu.ac.th>
2022-04-13 23:36:39 +0700

    [add] env

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, APIRouter, Request
+import uvicorn
+from fastapi import APIRouter, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-import uvicorn
 
 from build.api import build_router
+from utils.dbUtil import database
 
 app = FastAPI(
     docs_url='/docs',
@@ -14,6 +15,7 @@ app = FastAPI(
 )
 
 origins = [
+    "*",
     "http://localhost",
     "http://localhost:3000",
 ]
@@ -28,21 +30,22 @@ app.add_middleware(
 
 # TODO: Add routers
 v1_router = APIRouter(prefix="/api/v1")
-v1_router.include_router(build_router, tags=["Auth"])
+v1_router.include_router(build_router, tags=["Project"])
 app.include_router(v1_router)
 
 
-# @app.on_event('startup')
-# async def startup():
-#     await database.connect()
-#     print('DB is Connect!')
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+    print('DB is Connect!')
 
 
-# @app.on_event('shutdown')
-# async def shutdown():
-#     await database.disconnect()
-#     print('DB is Disconnect!')
+@app.on_event('shutdown')
+async def shutdown():
+    await database.disconnect()
+    print('DB is Disconnect!')
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True, workers=5, log_level="info")
+    uvicorn.run("main:app", host="127.0.0.1", port=4000,
+                reload=True, log_level="info")
